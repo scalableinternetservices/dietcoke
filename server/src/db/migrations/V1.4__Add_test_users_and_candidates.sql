b2853842f03ce183dbbc8e36600ada681974d189
Simon Zeiger <monzeiger@gmail.com>
2020-11-12 13:57:28 -0800

    Remove test migration

--- server/src/db/migrations/V1.4__Add_test_users_and_candidates.sql
+++
@@ -1,8 +0,0 @@
-insert into `candidate` (`name`, `voteCount`, `id`) values ('Bob', '2', 1);
-insert into `candidate` (`name`, `voteCount`, `id`) values ('Sue', '2', 2);
-insert into `candidate` (`name`, `voteCount`, `id`) values ('Bill', '1', 3);
-insert into `user` (`email`, `userType`, `name`, `candidateIds`) values ('email@email.com', 'ADMIN', 'John Fothels', '1,3,2');
-insert into `user` (`email`, `userType`, `name`, `candidateIds`) values ('email@email.com', 'ADMIN', 'John Kothels', '2,1,3');
-insert into `user` (`email`, `userType`, `name`, `candidateIds`) values ('email@email.com', 'ADMIN', 'John Mothels', '3,1,2');
-insert into `user` (`email`, `userType`, `name`, `candidateIds`) values ('email@email.com', 'ADMIN', 'John Jothels', '1,3,2');
-insert into `user` (`email`, `userType`, `name`, `candidateIds`) values ('rothfels@cs.ucla.edu', 'ADMIN', 'John Rothfels', '2,1,3');
