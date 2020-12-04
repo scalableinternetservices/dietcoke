47fdbc2dcc13279d587f9ba0421b72d502ac8f76
stevenplara <stevenplara@ucla.edu>
2020-12-04 12:20:24 -0800

    bespin cleanup

--- script/deploy-local.sh
+++ script/deploy-local.sh
@@ -16,22 +16,22 @@ echo "================================="
 aws ecr get-login-password | docker login --username AWS --password-stdin 101624687637.dkr.ecr.us-west-2.amazonaws.com
 
 echo "Building local docker image"
-docker build --tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:local .
+docker build --tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:local .
 
 echo "Pushing local docker image"
-docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:local
+docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:local
 
 echo "Updating previous and latest"
-docker pull 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:latest
-docker tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:latest 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:previous
-docker tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:local 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:latest
+docker pull 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:latest
+docker tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:latest 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:previous
+docker tag 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:local 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:latest
 
 echo "Pushing latest and previous"
-docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:latest
-docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/bespin:previous
+docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:latest
+docker push 101624687637.dkr.ecr.us-west-2.amazonaws.com/dietcoke:previous
 
 echo "Updating app-web"
-./script/deploy-ecs.sh bespin-app-web "local"
+./script/deploy-ecs.sh dietcoke-app-web "local"
 
 # echo "Updating app-background"
 # ./script/deploy-ecs.sh app-background "local"
@@ -40,7 +40,7 @@ echo "Updating app-web"
 # aws lambda update-function-code \
 #   --no-cli-pager \
 #   --region us-west-2 \
-#   --function-name bespin \
+#   --function-name dietcoke \
 #   --zip-file fileb://./server/bundle.zip
 
 echo "DONE"
